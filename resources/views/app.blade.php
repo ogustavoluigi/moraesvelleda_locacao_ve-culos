a4aea7d2cc4f0c50ef84db24f7e44127f8f47f95
Gustavo Luigi <=>
2024-07-08 11:58:10 -0300

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,13 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="icon" href="{{ asset('moraes-velleda_favicon_16px.png') }}" type="image/png" sizes="16x16"/>
+        <link rel="icon" href="{{ asset('moraes-velleda_favicon_32px.png') }}" type="image/png" sizes="32x32"/>
+        <link rel="apple-touch-icon" href="{{ asset('moraes-velleda_favicon_60px.png') }}">
+        <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('moraes-velleda_favicon_76px.png') }}">
+        <link rel="apple-touch-icon" sizes="120x120" href="{{ asset('moraes-velleda_favicon_120px.png') }}">
+        <link rel="apple-touch-icon" sizes="152x152" href="{{ asset('moraes-velleda_favicon_152px.png') }}">
+
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
